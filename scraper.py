87345b550070f91d04b4b29574d4d5a71b27e431
natashk <box240308@yandex.ru>
2023-01-11 21:54:51 -0700

    Removed index and extra line in title

--- scraper.py
+++ scraper.py
@@ -1,12 +1,15 @@
 import requests
 from datetime import date
+import pandas as pd
 
 if __name__ == "__main__":
+    file_name = 'FRB_H15.csv'
     start_date = '01/01/1954'
     end_date = date.today().strftime("%m/%d/%Y")
     url = 'https://www.federalreserve.gov/datadownload/Output.aspx?rel=H15&series=c5025f4bbbed155a6f17c587772ed69e&lastobs=&from='+start_date+'&to='+end_date+'&filetype=csv&label=omit&layout=seriescolumn'
     r = requests.get(url)
     if r.status_code == 200:
-        file = open('FRB_H15.csv','wb')
-        file.write(r.content)
-        file.close
+        with open(file_name,'wb') as file:
+            file.write(r.content)
+            df = pd.read_csv(file_name, skiprows=1)
+            df.to_csv(file_name, index=False)
