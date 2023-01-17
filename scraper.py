590d77a6df3b5822f28becc08c111fa5cfc74740
natashk <box240308@yandex.ru>
2023-01-17 12:25:27 -0700

    Fixed file opening mode

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = 'https://www.federalreserve.gov/datadownload/Output.aspx?rel=H15&series=c5025f4bbbed155a6f17c587772ed69e&lastobs=&from='+start_date+'&to='+end_date+'&filetype=csv&label=omit&layout=seriescolumn'
     r = requests.get(url)
     if r.status_code == 200:
-        with open(file_name,'wb') as file:
+        with open(file_name,'w', encoding='utf-8') as file:
             file.write(r.content)
             df = pd.read_csv(file_name, skiprows=1)
             df.to_csv(file_name, index=False)
